562d6c6a16707a56cce2f0494d1ed6fc531eac3a
John Doe <ajflory9@yahoo.com>
2018-09-22 18:08:50 -0400

    Components added

--- src/Components/landingpage.js
+++ src/Components/landingpage.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render(){
         return(
-                <div>
-                    
-                </div>
+            <Grid className="demo-grid-2">
+                <Cell col={6}>6</Cell>
+                <Cell col={4}>4</Cell>
+                <Cell col={2}>2</Cell>
+            </Grid>
             )
     }
         
