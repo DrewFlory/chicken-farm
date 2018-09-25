ddf4e0ebd8a64505509bf3fa7e566c174c7dd8e0
John Doe <ajflory9@yahoo.com>
2018-09-25 11:56:15 -0400

    Added Pricing Modal to products page

---
+++ src/Components/drumstick.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import { Button, Dialog, DialogTitle, DialogActions, DialogContent} from 'react-mdl';
+
+class Drum extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+      this.handleOpenDialog = this.handleOpenDialog.bind(this);
+      this.handleCloseDialog = this.handleCloseDialog.bind(this);
+    }
+  
+    handleOpenDialog() {
+      this.setState({
+        openDialog: true
+      });
+    }
+  
+    handleCloseDialog() {
+      this.setState({
+        openDialog: false
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <Button onClick={this.handleOpenDialog} style={{backgroundColor: "white", borderRadius: "20px", marginLeft: "25%", marginTop: "5%"}}raised ripple>Pricing</Button>
+          <Dialog open={this.state.openDialog}>
+            <DialogTitle>Allow data collection?</DialogTitle>
+            <DialogContent>
+              <p>Allowing us to collect data will let us get you the information you want faster.</p>
+            </DialogContent>
+            <DialogActions>
+              <Button type='button' onClick={this.handleCloseDialog}>Close</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
+    }
+  }
+
+  export default Drum;
--- src/Components/products.js
+++ src/Components/products.js
@@ -1,37 +1,61 @@
 import React, {Component} from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Whole from './whole';
+import Leg from './leg';
+import Drum from './drumstick';
+import Thigh from './thigh';
+import Breast from './breast';
+import Wings from './wings';
+
 
 class Product extends Component {
+        
     render(){
         return(
+            
                 <div className = "products">
+                    <h1>Our Products</h1>
                     <Grid className="demo-grid-1">
                         <Cell col={4}>
+                        <div className="product-card">
+                            <h4>Whole</h4>
                             <img className="product-img" src="https://fustany.com/images/en/content/header_image_fustany-lifestyle-the_kitchen-roasted_chicken_dressing_recipe-main_image.png"></img>
+                            <Whole />
+                        </div>
                         </Cell>
                         <Cell col={4}>
+                            <h4>Leg Quarters</h4>
                             <img className="product-img" src="https://mealplannerpro.com/images/recipes/recipes/0/324/324068/713870.jpg"></img>
+                            <Leg />
                         </Cell>
                         <Cell col={4}>
+                            <h4>Drumstick</h4>
                             <img className="product-img" src="http://picturetherecipe.com/wp-content/uploads/2013/06/Grilled-Beer-Marinated-Chicken-Drumsticks-by-PictureTheRecipe.jpg"></img>
+                            <Drum />
                         </Cell>
                     </Grid>
 
                     <Grid className="demo-grid-2">
                         <Cell col={4}>
+                            <h4>Thigh</h4>
                             <img className="product-img" src="https://grillgirl.com/wp-content/uploads/2011/05/DSC_0858-1280x640.jpg"></img>
+                            <Thigh />
                         </Cell>
                         <Cell col={4}>
+                            <h4>Boneless Breast</h4>
                             <img className="product-img" src="https://www.tasteofhome.com/wp-content/uploads/2017/10/Grilled-Barbecued-Chicken_exps24848_UGG143377C01_23_3bC_RMS-696x696.jpg"></img>
+                            <Breast />
                         </Cell>
                         <Cell col={4}>
+                            <h4>Whole Wing</h4>
                             <img className="product-img" src="http://2.bp.blogspot.com/_VKAtSAnv1yU/TBxdzksFP7I/AAAAAAAAC9I/G8YJ-xJNC28/s1600/blogger194.JPG"></img>
+                            <Wings />
                         </Cell>
                     </Grid>
                 </div>
             )
     }
-        
+    
 }
 
 export default Product;
